5a33f35bf70af817d314b59d7c208e340010edca
niklsspb <prof051@yandex.ru>
2016-12-16 12:18:18 +0300

    Update quadratic_equation.py
    Решение задачи №7

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,9 +3,14 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant == 0:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
         return root1, None
+    elif discriminant < 0:
+        # При отрицательном дискриминанте тоже корни есть, они просто комплексные
+        # (-b - discriminant) / (2 * a), (-b + discriminant) / (2 * a)
+        return None, None
     else:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
         return root1, root2
